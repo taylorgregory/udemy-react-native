788e804d755b9c91185bd2258642a02e42085486
taylor <taylorgrgry@gmail.com>
2021-12-09 16:25:06 +1000

    structural changes to repository to account for multiple small projects

---
+++ course-goals-app/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, TextInput, View, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View style={{ padding: 60 }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+        <TextInput
+          placeholder="Course Goal"
+          style={{ width: '80%', borderColor: "black", borderWidth: 1, padding: 10 }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
