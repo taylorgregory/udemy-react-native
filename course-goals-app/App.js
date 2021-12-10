f5bb300399924fd866eabb63449af439cb608a18
taylor <taylorgrgry@gmail.com>
2021-12-10 16:02:14 +1000

    finished the course goals app

--- course-goals-app/App.js
+++ course-goals-app/App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, TextInput, View, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Button,
+  FlatList,
+} from "react-native";
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+  const addGoalHandler = (goalTitle) => {
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: goalTitle },
+    ]);
+    setIsAddMode(false);
+  };
+
+  const removeGoalHandler = (goalId) => {
+    setCourseGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== goalId);
+    });
+  };
+
+  const cancelGoalAdditionHandler = () => {
+    setIsAddMode(false);
+  };
+
   return (
-    <View style={{ padding: 60 }}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-        <TextInput
-          placeholder="Course Goal"
-          style={{ width: '80%', borderColor: "black", borderWidth: 1, padding: 10 }}
-        />
-        <Button title="ADD" />
-      </View>
-      <View></View>
+    <View style={styles.screen}>
+      <Button title='Add New Goal' onPress={() => setIsAddMode(true)}/>
+      <GoalInput visible={isAddMode} onAddGoal={addGoalHandler} onCancel={cancelGoalAdditionHandler} />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <GoalItem title={itemData.item.value} id={itemData.item.id} onDelete={removeGoalHandler} />
+        )}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 60,
+  },
+});
