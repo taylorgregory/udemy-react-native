4d29c600521db16d7a37c737da65f996734ad647
taylor <taylorgrgry@gmail.com>
2021-12-17 12:26:10 +1000

    adding specific meal info

--- the-meals-app/screens/CategoriesMealsScreen.js
+++ the-meals-app/screens/CategoriesMealsScreen.js
@@ -1,25 +1,42 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, Platform } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
+import { View, StyleSheet, FlatList } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 const CategoriesMealsScreen = (props) => {
-  const catId = props.navigation.getParam("categoryId");
-  const selectedCategory = CATEGORIES.find((cat) => cat.id == catId);
-  return (
-    <View style={styles.screen}>
-      <Text>The Categories Meals Screen!</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button
-        title="Go to Meal Detail!"
-        onPress={() => {
-          props.navigation.navigate({ routeName: "MealDetail" });
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: "MealDetail",
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
         }}
       />
-      <Button
-        title="Go Back"
-        onPress={() => {
-          props.navigation.pop();
-        }}
+    );
+  };
+
+  const catId = props.navigation.getParam("categoryId");
+
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  return (
+    <View style={styles.screen}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: "95%" }}
       />
     </View>
   );
--- the-meals-app/screens/MealDetailScreen.js
+++ the-meals-app/screens/MealDetailScreen.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { MEALS } from "../data/dummy-data";
 
 const MealDetailScreen = (props) => {
-    return (
-        <View style={styles.screen}>
-          <Text>The Meal Detail Screen!</Text>
-          <Button title="Go Back to Categories" onPress={() => {
-            props.navigation.popToTop();
-          }} />
-        </View>
-      );
+  const mealId = props.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  return (
+    <View style={styles.screen}>
+      <Text>{selectedMeal.title}</Text>
+      <Button
+        title="Go Back to Categories"
+        onPress={() => {
+          props.navigation.popToTop();
+        }}
+      />
+    </View>
+  );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => (meal.id = mealId));
+  return {
+    headerTitle: selectedMeal.title,
+  };
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default MealDetailScreen;
