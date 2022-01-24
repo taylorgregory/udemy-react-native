ea598c18e0afc46d925c4a8a05b6f5c220743b6e
taylor <taylorgrgry@gmail.com>
2022-01-24 14:00:29 +1000

    initial setup of the shop app

---
+++ the-shop-app/screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+// Presenting all of the products that we can order
+
+import React from 'react';
+import { FlatList } from 'react-native';
+
+const ProductsOverviewScreen = (props) => {
+    return <FlatList />;
+};
+
+export default ProductsOverviewScreen;
